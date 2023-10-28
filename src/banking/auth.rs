93df299de47dcadb82867a0a7c17b3d2b8a96343
floooptimism <jackmord445@gmail.com>
2023-10-28 14:07:41 +0800

    hashing

--- src/banking/auth.rs
+++ src/banking/auth.rs
@@ -1,3 +1,6 @@
+use std::io::Write;
+use std::string::FromUtf8Error;
+use sha256::digest;
 use crate::banking::types::ID;
 
 pub struct Credential {
@@ -11,7 +14,28 @@ impl Credential {
         Credential {
             account_id: id,
             username,
-            password
+            password: hash(password, None)
         }
+    }
+}
+
+// **
+pub fn hash(input: String, salt: Option<String>) -> String {
+    let mut input = input;
+    if let Some(salt) = salt {
+        input += salt.as_str();
+    }
+    digest(input)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::hash;
+    #[test]
+    fn hash_test() {
+        let result = hash("testing".to_string(), None);
+        println!("{}", result);
+        assert_eq!(result, "cf80cd8aed482d5d1527d7dc72fceff84e6326592848447d2dc0b0e87dfc9a90".to_string());
     }
 }
